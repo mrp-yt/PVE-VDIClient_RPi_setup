3c1662689445eb6c472ce7090339a683a7f7605c
mrp-yt <88549505+mrp-yt@users.noreply.github.com>
2023-08-11 20:23:17 +0100

    Update vdiclient_setup_script_1.sh

--- vdiclient_setup_script_1.sh
+++ vdiclient_setup_script_1.sh
@@ -1,1 +1,10 @@
+cd ~ &&
+sudo apt update && sudo apt upgrade -y &&
+sudo apt install xserver-xorg lxde-core lxappearance python3-pip python3-tk virt-viewer git -y &&
+pip3 install proxmoxer &&
+pip3 install PySimpleGUI &&
+pip3 install requests &&
+sudo git clone https://github.com/joshpatten/PVE-VDIClient.git && 
+sudo chmod +x ~/PVE-VDIClient/vdiclient.py &&
+touch thinclient
 
